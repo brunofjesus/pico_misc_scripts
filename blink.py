aacc077a965b1d7ee28fffddeb48797f9c448199
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-07-12 23:24:23 +0100

    Blink: list of leds in sequence

--- blink.py
+++ blink.py
@@ -1,9 +1,26 @@
 from machine import Pin
 from time import sleep
 
-led = Pin("LED", Pin.OUT)
+ledPins = ["LED", 14, 15]
+ledObjs = [];
+
+def initLeds():
+    for led in ledPins:
+        led = Pin(led, Pin.OUT)
+        led.value(0)
+        ledObjs.append(led)
+        
+def toggleLeds(ledList: list, interval :int):
+    for led in ledList:
+        print(led)
+        led.toggle()
+        sleep(interval)
+
+
+initLeds()
 
 while True:
-    led.toggle()
+    toggleLeds(ledObjs, 0.2)
+    sleep(0.5)
+    toggleLeds(reversed(ledObjs), 0.2)
     sleep(2)
-    print("Hello world");
