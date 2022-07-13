18737ddab1e0458b16e73ebc7787c4dc621ce205
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-07-13 20:23:42 +0100

    Blink: Keep pressing to light all

--- blink.py
+++ blink.py
@@ -20,18 +20,38 @@ class SequentialLed():
         for led in ledList:
             led.toggle()
             sleep(interval)
+            
+    def toggleNextUp(self):
+        for led in self.ledObjs:
+            if led.value() == 0:
+                led.value(1)
+                break
+    
+    def toggleNextDown(self):
+        for led in reversed(self.ledObjs):
+            if led.value() == 1:
+                led.value(0)
+                break
+                
+    def toggleNext(self, onElseOff :bool):
+        if onElseOff:
+            self.toggleNextUp()
+        else:
+            self.toggleNextDown()
+        
+        
 
 
 button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 
-sequentialLed = SequentialLed([13, 14, 15, "LED"])
-reverse = False
+sequentialLed = SequentialLed(["LED", 15, 14, 13])
 sequentialLed.toggle(0.2,False)
 sequentialLed.toggle(0.2,True)
+idx = 0;
 
-while True:        
-    if (button.value() == 1 and not reverse) or (button.value() == 0 and reverse):
-        sequentialLed.toggle(0.2, reverse)
-        reverse = not reverse
-        sleep(0.5)
+while True:
+    idx = sequentialLed.toggleNext(button.value() == 1)
+    sleep(0.2)
+
+        
