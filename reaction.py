fa3f1d549b32327cdea6eec9ce0a8b4181cc222a
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-07-14 00:28:35 +0100

    Reaction: Add waiting led blink

--- reaction.py
+++ reaction.py
@@ -1,22 +1,50 @@
 import machine
 import urandom
 import utime
+import _thread
 
-pressed = False
-led = machine.Pin(15, machine.Pin.OUT)
+class Game():
+    
+    def __init__(self):
+        self.playing = False
+        self.pressed = False
+        self.led = machine.Pin(15, machine.Pin.OUT)
+        self.button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_DOWN)
+        self.led_waiting = machine.Pin(14, machine.Pin.OUT)
+        self.button.irq(trigger=machine.Pin.IRQ_RISING, handler=self.button_handler)
+        _thread.start_new_thread(Game.wait, (self,))
 
-button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_DOWN)
+    def button_handler(self, pin):        
+        if not self.pressed and self.playing:
+            self.pressed = True
+            self.playing = False
+            timer_reaction = utime.ticks_diff(utime.ticks_ms(), self.timer_start)
+            print("Your reaction time was " + str(timer_reaction) + " ms!")
+            utime.sleep(1)
+            print("Click to play again!")
+        elif not self.playing:
+            self.play()
+            
+    def play(self):
+        self.playing = True
+        self.led_waiting.value(0)
+        self.led.value(1)
+        utime.sleep(urandom.uniform(5,10))
+        self.led.value(0)
 
-def button_handler(pin):
-    global pressed
-    if not pressed:
-        pressed=True
-    timer_reaction = utime.ticks_diff(utime.ticks_ms(), timer_start)
-    print("Your reaction time was " + str(timer_reaction) + " ms!")
+        self.timer_start = utime.ticks_ms()
+
+
+    def wait(game):
+        while True:
+            if not game.playing:
+                game.led_waiting.value(1)
+                utime.sleep(1)
+                game.led_waiting.value(0)
+            utime.sleep(0.5)
         
-led.value(1)
-utime.sleep(urandom.uniform(5,10))
-led.value(0)
 
-timer_start = utime.ticks_ms()
-button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+game = Game()
+
+
+        
